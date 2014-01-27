422a08ad51542e119d100f7abe353848ac71be5f
Eric Wittmann <eric.wittmann@gmail.com>
2014-01-27 11:41:53 -0500

    fixed a typo in defaultproxy.sh

---
+++ testing/client/direct.sh
@@ -0,0 +1,1 @@
+mvn exec:java -Durl=http://localhost:8080/testingservice/testing/echo?value=hello1 -Dvalue=hello -Dcount=10000
--- testing/proxy/defaultproxy.sh
+++ testing/proxy/defaultproxy.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Durl=http://localhost:8080/testinervice/testing/echo?value=hello1
+mvn exec:java -Durl=http://localhost:8080/testingservice/testing/echo?value=hello1
