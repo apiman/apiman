4287f5f325b704d7af8c181330ab973fac7377a5
Eric Wittmann <eric.wittmann@gmail.com>
2014-07-16 09:40:42 -0400

    https://issues.jboss.org/browse/APIMAN-90

--- apiman-dt/apiman-dt-ui/apiman-dt-ui-war/src/main/java/org/overlord/apiman/dt/ui/client/local/pages/PlanPoliciesPage.java
+++ apiman-dt/apiman-dt-ui/apiman-dt-ui-war/src/main/java/org/overlord/apiman/dt/ui/client/local/pages/PlanPoliciesPage.java
@@ -15,13 +15,20 @@
  */
 package org.overlord.apiman.dt.ui.client.local.pages;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.overlord.apiman.dt.api.beans.policies.PolicyBean;
 import org.overlord.apiman.dt.ui.client.local.AppMessages;
+import org.overlord.apiman.dt.ui.client.local.events.RemovePolicyEvent;
+import org.overlord.apiman.dt.ui.client.local.pages.common.PolicyList;
+import org.overlord.apiman.dt.ui.client.local.services.rest.IRestInvokerCallback;
 import org.overlord.apiman.dt.ui.client.local.util.MultimapUtil;
 
 import com.google.gwt.user.client.ui.Anchor;
@@ -37,24 +44,63 @@ import com.google.gwt.user.client.ui.Anchor;
 @Dependent
 public class PlanPoliciesPage extends AbstractPlanPage {
 
+    private List<PolicyBean> policyBeans;
+
     @Inject @DataField
     Anchor toNewPolicy;
+    @Inject @DataField
+    PolicyList policies;
     
     /**
      * Constructor.
      */
     public PlanPoliciesPage() {
     }
-    
+
+    /**
+     * Called after the bean is created.
+     */
+    @PostConstruct
+    protected void postConstruct() {
+        policies.addRemovePolicyHandler(new RemovePolicyEvent.Handler() {
+            @Override
+            public void onRemovePolicy(RemovePolicyEvent event) {
+                doRemovePolicy(event.getPolicy());
+            }
+        });
+    }
+
     /**
      * @see org.overlord.apiman.dt.ui.client.local.pages.AbstractPage#loadPageData()
      */
     @Override
     protected int loadPageData() {
         int rval = super.loadPageData();
-        return rval;
+        // we'll trigger an additional load after the service version has been loaded (hence the +1)
+        return rval + 1;
     }
-
+    
+    /**
+     * @see org.overlord.apiman.dt.ui.client.local.pages.AbstractPlanPage#onPlanVersionLoaded()
+     */
+    @Override
+    protected void onPlanVersionLoaded() {
+        String orgId = org;
+        String planId = plan;
+        String planVersion = versionBean.getVersion();
+        rest.getServicePolicies(orgId, planId, planVersion, new IRestInvokerCallback<List<PolicyBean>>() {
+            @Override
+            public void onSuccess(List<PolicyBean> response) {
+                policyBeans = response;
+                dataPacketLoaded();
+            }
+            @Override
+            public void onError(Throwable error) {
+                dataPacketError(error);
+            }
+        });
+    }
+    
     /**
      * @see org.overlord.apiman.dt.ui.client.local.pages.AbstractAppPage#renderPage()
      */
@@ -66,6 +112,26 @@ public class PlanPoliciesPage extends AbstractPlanPage {
                 MultimapUtil.fromMultiple("org", this.planBean.getOrganizationId(), "id", this.planBean.getId(),  //$NON-NLS-1$ //$NON-NLS-2$
                         "ver", this.versionBean.getVersion(), "type", "app")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         toNewPolicy.setHref(newPolicyHref);
+        policies.setValue(policyBeans);
+    }
+
+    /**
+     * Called when the user chooses to remove a policy.
+     * @param policy
+     */
+    protected void doRemovePolicy(final PolicyBean policy) {
+        rest.removePolicy(policy.getType(), org, versionBean.getPlan().getId(), versionBean.getVersion(),
+                policy.getId(), new IRestInvokerCallback<Void>() {
+            @Override
+            public void onSuccess(Void response) {
+                policyBeans.remove(policy);
+                policies.setValue(policyBeans);
+            }
+            @Override
+            public void onError(Throwable error) {
+                dataPacketError(error);
+            }
+        });
     }
 
     /**
