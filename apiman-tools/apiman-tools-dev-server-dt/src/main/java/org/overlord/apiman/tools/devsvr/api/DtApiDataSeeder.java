95a6364f1de960ef9cb5f759768bf257daf721f3
Eric Wittmann <eric.wittmann@gmail.com>
2014-09-29 15:08:41 -0400

    removed some debugging code that accidentally got included before

--- apiman-tools/apiman-tools-dev-server-dt/src/main/java/org/overlord/apiman/tools/devsvr/api/DtApiDataSeeder.java
+++ apiman-tools/apiman-tools-dev-server-dt/src/main/java/org/overlord/apiman/tools/devsvr/api/DtApiDataSeeder.java
@@ -60,10 +60,6 @@ public class DtApiDataSeeder extends DefaultTestDataSeeder {
     public void seed(IIdmStorage idmStorage, IStorage storage) throws StorageException {
         super.seed(idmStorage, storage);
         
-        if (Boolean.TRUE) {
-            return;
-        }
-        
         // Create Organization Owner role
         RoleBean role = new RoleBean();
         role.setId("OrganizationOwner"); //$NON-NLS-1$
