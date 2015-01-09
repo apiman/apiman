fecbd7d225ab53517fcc2666084fac27b70491a2
Eric Wittmann <eric.wittmann@gmail.com>
2015-01-09 15:48:29 -0500

    added a very simple no-op policy plugin - only useful as a starting point for setting up a plugin maven project

---
+++ noop-policy/src/main/java/io/apiman/plugins/noop_policy/NoopPolicy.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.apiman.plugins.noop_policy;
+
+import io.apiman.gateway.engine.beans.ServiceRequest;
+import io.apiman.gateway.engine.beans.ServiceResponse;
+import io.apiman.gateway.engine.beans.exceptions.ConfigurationParseException;
+import io.apiman.gateway.engine.policy.IPolicy;
+import io.apiman.gateway.engine.policy.IPolicyChain;
+import io.apiman.gateway.engine.policy.IPolicyContext;
+
+/**
+ * A policy that does nothing.  But hey, at least it never fails!
+ *
+ * @author eric.wittmann@redhat.com
+ */
+public class NoopPolicy implements IPolicy {
+
+    /**
+     * Constructor.
+     */
+    public NoopPolicy() {
+    }
+
+    /**
+     * @see io.apiman.gateway.engine.policy.IPolicy#parseConfiguration(java.lang.String)
+     */
+    @Override
+    public Object parseConfiguration(String jsonConfiguration) throws ConfigurationParseException {
+        return null;
+    }
+
+    /**
+     * @see io.apiman.gateway.engine.policy.IPolicy#apply(io.apiman.gateway.engine.beans.ServiceRequest, io.apiman.gateway.engine.policy.IPolicyContext, java.lang.Object, io.apiman.gateway.engine.policy.IPolicyChain)
+     */
+    @Override
+    public void apply(ServiceRequest request, IPolicyContext context, Object config,
+            IPolicyChain<ServiceRequest> chain) {
+        chain.doApply(request);
+    }
+
+    /**
+     * @see io.apiman.gateway.engine.policy.IPolicy#apply(io.apiman.gateway.engine.beans.ServiceResponse, io.apiman.gateway.engine.policy.IPolicyContext, java.lang.Object, io.apiman.gateway.engine.policy.IPolicyChain)
+     */
+    @Override
+    public void apply(ServiceResponse response, IPolicyContext context, Object config,
+            IPolicyChain<ServiceResponse> chain) {
+        chain.doApply(response);
+    }
+
+}
