1e248bfb65ea2b971346478016b068fe242c2bd0
Eric Wittmann <eric.wittmann@gmail.com>
2015-01-19 08:54:32 -0500

    added a common plugin module

---
+++ common/plugin/src/main/java/io/apiman/common/plugin/PluginClassLoader.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.apiman.common.plugin;
+
+import java.io.File;
+
+/**
+ * A classloader that is capable of loading classes from an apiman
+ * plugin artifact.
+ *
+ * @author eric.wittmann@redhat.com
+ */
+public class PluginClassLoader extends ClassLoader {
+    
+    private File pluginArtifact;
+
+    /**
+     * Constructor.
+     * @param pluginArtifact
+     */
+    public PluginClassLoader(File pluginArtifact) {
+        this.pluginArtifact = pluginArtifact;
+    }
+    
+}
