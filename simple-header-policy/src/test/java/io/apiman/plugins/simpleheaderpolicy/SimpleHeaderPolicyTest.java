a92622fb0301a7dd4c6d755e14a0b23dc42d81cd
Marc Savy <marc@rhymewithgravy.com>
2015-03-19 09:54:22 +0000

    Tests for overwrite to demonstrate functionality

--- simple-header-policy/src/test/java/io/apiman/plugins/simpleheaderpolicy/SimpleHeaderPolicyTest.java
+++ simple-header-policy/src/test/java/io/apiman/plugins/simpleheaderpolicy/SimpleHeaderPolicyTest.java
@@ -228,6 +228,44 @@ public class SimpleHeaderPolicyTest {
         assertFalse(request.getHeaders().containsKey("vetinari"));
         assertTrue(request.getHeaders().isEmpty());
     }
+    
+    @Test
+    public void shouldNotOverwrite() {
+        request.getHeaders().put("nobby", "nobbs");
+        
+        AddHeaderBean ahb = new AddHeaderBean();
+        ahb.setHeaderName("nobby");
+        ahb.setHeaderValue("sgt-detritus");
+        ahb.setApplyTo(ApplyTo.REQUEST);
+        
+        ahb.setOverwrite(false); // DO NOT OVERWRITE
+        
+        config.getAddHeaders().add(ahb);
+        
+        policy.apply(request, mContext, config, mRequestChain);
+        
+        assertEquals("nobbs", request.getHeaders().get("nobby"));
+        assertEquals(1, request.getHeaders().size());
+    }
+    
+    @Test
+    public void shouldOverwrite() {
+        request.getHeaders().put("nobby", "nobbs");
+        
+        AddHeaderBean ahb = new AddHeaderBean();
+        ahb.setHeaderName("nobby");
+        ahb.setHeaderValue("sgt-detritus");
+        ahb.setApplyTo(ApplyTo.REQUEST);
+        
+        ahb.setOverwrite(true); // OVERWRITE
+        
+        config.getAddHeaders().add(ahb);
+        
+        policy.apply(request, mContext, config, mRequestChain);
+        
+        assertEquals("sgt-detritus", request.getHeaders().get("nobby"));
+        assertEquals(1, request.getHeaders().size());
+    }
 
     // @Test
     // public void shouldTrimWhitespace() {
