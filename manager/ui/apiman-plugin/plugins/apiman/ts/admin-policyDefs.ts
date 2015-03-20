c29fd0e416de41f1d5cea795fe5f799b5862d82c
Eric Wittmann <eric.wittmann@gmail.com>
2015-03-20 07:40:35 -0400

    added filtering to the policy defs page

--- manager/ui/apiman-plugin/plugins/apiman/ts/admin-policyDefs.ts
+++ manager/ui/apiman-plugin/plugins/apiman/ts/admin-policyDefs.ts
@@ -4,9 +4,26 @@ module Apiman {
 
     export var AdminPolicyDefsController = _module.controller("Apiman.AdminPolicyDefsController",
         ['$q', '$scope', 'ApimanSvcs', 'PageLifecycle', ($q, $scope, ApimanSvcs, PageLifecycle) => {
+            
+            $scope.filterPolicies = function(value) {
+                if (!value) {
+                    $scope.filteredPolicyDefs = $scope.policyDefs;
+                } else {
+                    var filtered = [];
+                    for (var i = 0; i < $scope.policyDefs.length; i++) {
+                        var policyDef = $scope.policyDefs[i];
+                        if (policyDef.name.toLowerCase().indexOf(value) > -1) {
+                            filtered.push(policyDef);
+                        }
+                    }
+                    $scope.filteredPolicyDefs = filtered;
+                }
+            };
+            
             var promise = $q.all({
                 policyDefs: $q(function(resolve, reject) {
                     ApimanSvcs.query({ entityType: 'policyDefs' }, function(policyDefs) {
+                        $scope.filteredPolicyDefs = policyDefs;
                         resolve(policyDefs);
                     }, function(error) {
                         reject(error);
