ef00e9b18127aae76b651c9f3c6402b0298b59e6
Marc Savy <marc@rhymewithgravy.com>
2015-03-23 12:23:31 +0000

    Finish servlet impl

--- gateway/platforms/servlet/src/main/java/io/apiman/gateway/platforms/servlet/components/PeriodicComponentImpl.java
+++ gateway/platforms/servlet/src/main/java/io/apiman/gateway/platforms/servlet/components/PeriodicComponentImpl.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.apiman.gateway.platforms.servlet.components;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -9,51 +24,84 @@ import java.util.concurrent.atomic.AtomicInteger;
 import io.apiman.gateway.engine.async.IAsyncHandler;
 import io.apiman.gateway.engine.components.IPeriodicComponent;
 
+/**
+ * Servlet implementation of {@link IPeriodicComponent} aiming to be non-blocking.
+ * 
+ * A unique IDs are generated for each timer via a non-blocking {@link AtomicInteger}, with the (unlikely to
+ * be achieved) limitation that these will eventually start overwriting once the bounds of {@link Long} are
+ * surpassed.
+ * 
+ * @author Marc Savy <msavy@redhat.com>
+ */
 public class PeriodicComponentImpl implements IPeriodicComponent {
-    
     private Timer timer = new Timer();
-    private Set<Long> idSet = new LinkedHashSet<>();
-    private AtomicInteger id;
- 
+    private Map<Long, TimerTask> timerTaskMap = new LinkedHashMap<>();
+    private AtomicInteger id = new AtomicInteger(0);
+
     public PeriodicComponentImpl() {
     }
 
+    /**
+     * @see io.apiman.gateway.engine.components.IPeriodicComponent#setPeriodicTimer(long, long,
+     *      io.apiman.gateway.engine.async.IAsyncHandler)
+     */
     @Override
     public long setPeriodicTimer(long periodMillis, long initialDelayMillis,
             final IAsyncHandler<Long> periodicHandler) {
-        
-        AtomicInteger id = new AtomicInteger();
-        
+        final long timerId = id.incrementAndGet();
+
         TimerTask task = new TimerTask() {
 
             @Override
             public void run() {
-                periodicHandler.handle(result);
+                periodicHandler.handle(timerId);
             }
         };
-        
-        task.
-        
-        timer.schedule(null, initialDelayMillis, periodMillis);
-
+        // Keep reference to TimerTask and Timer ID we hand back (to cancel later).
+        timerTaskMap.put(timerId, task);
+        // Task, delay, frequency
+        timer.schedule(task, initialDelayMillis, periodMillis);
+        return timerId;
     }
 
+    /**
+     * @see io.apiman.gateway.engine.components.IPeriodicComponent#setOneshotTimer(long,
+     *      io.apiman.gateway.engine.async.IAsyncHandler)
+     */
     @Override
-    public long setOneshotTimer(long deltaMillis, IAsyncHandler<Long> timerHandler) {
-        // TODO Auto-generated method stub
-        return 0;
+    public long setOneshotTimer(long deltaMillis, final IAsyncHandler<Long> timerHandler) {
+        final long timerId = id.incrementAndGet();
+
+        TimerTask task = new TimerTask() {
+
+            @Override
+            public void run() {
+                timerHandler.handle(timerId);
+            }
+        };
+        // Keep reference to TimerTask and Timer ID we hand back (to cancel later).
+        timerTaskMap.put(timerId, task);
+        // Task, delay, frequency
+        timer.schedule(task, deltaMillis);
+        return timerId;
     }
 
+    /**
+     * @see io.apiman.gateway.engine.components.IPeriodicComponent#cancelTimer(long)
+     */
     @Override
     public void cancelTimer(long timerId) {
-        // TODO Auto-generated method stub
-
+        timerTaskMap.remove(timerId).cancel();
     }
 
+    /**
+     * @see io.apiman.gateway.engine.components.IPeriodicComponent#cancelAll()
+     */
     @Override
     public void cancelAll() {
-        // TODO Auto-generated method stub
-        
+        for (TimerTask timer : timerTaskMap.values()) {
+            timer.cancel();
+        }
+        timerTaskMap.clear();
     }
-
 }
