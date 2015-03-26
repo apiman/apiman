8b0c61bd8a47d0c865f7762419a7df78b7f5dd3c
Eric Wittmann <eric.wittmann@gmail.com>
2015-03-26 12:49:30 -0400

    fixed up broken/missing functionality on the service-contracts page

--- manager/ui/apiman-plugin/plugins/apiman/ts/service/service-contracts.ts
+++ manager/ui/apiman-plugin/plugins/apiman/ts/service/service-contracts.ts
@@ -2,36 +2,52 @@
 /// <reference path="../services.ts"/>
 module Apiman {
 
- export var ServiceContractsController = _module.controller("Apiman.ServiceContractsController",
-        ['$q', '$scope', '$location', 'PageLifecycle', 'ServiceEntityLoader', 'OrgSvcs',
-         ($q, $scope, $location, PageLifecycle, ServiceEntityLoader, OrgSvcs) => {
+    export var ServiceContractsController = _module.controller("Apiman.ServiceContractsController",
+        ['$q', '$scope', '$location', 'PageLifecycle', 'ServiceEntityLoader', 'OrgSvcs', 'Logger',
+        ($q, $scope, $location, PageLifecycle, ServiceEntityLoader, OrgSvcs, Logger) => {
             var params = $location.search();
             $scope.organizationId = params.org;
             $scope.tab = 'contracts';
             $scope.version = params.version;
-            
+
             var getNextPage = function(successHandler, errorHandler) {
                 var maxCount = 10;
                 $scope.currentPage = $scope.currentPage + 1;
                 OrgSvcs.query({ organizationId: params.org, entityType: 'services', entityId: params.service, versionsOrActivity: 'versions', version: params.version, policiesOrActivity: 'contracts', page: $scope.currentPage, count: maxCount  }, function(contracts) {
-                   if (contracts.length == maxCount) $scope.hasMore = true;
-                   else $scope.hasMore = false;
+                   if (contracts.length == maxCount) {
+                       $scope.hasMore = true;
+                   } else {
+                       $scope.hasMore = false;
+                   }
                    successHandler(contracts);
                 }, errorHandler);
             };
-             
+
             var dataLoad = ServiceEntityLoader.getCommonData($scope, $location);
-            if (params.version != null) {
-                dataLoad = angular.extend(dataLoad, {
-                    contracts: $q(function(resolve, reject) {
-                        $scope.currentPage = 0;
-                        getNextPage(resolve, reject);
-                    })
-                });
-            }
+            dataLoad = angular.extend(dataLoad, {
+                contracts: $q(function(resolve, reject) {
+                    $scope.currentPage = 0;
+                    getNextPage(resolve, reject);
+                }),
+                serviceVersion: $q(function(resolve, reject) {
+                    OrgSvcs.get({ organizationId: params.org, entityType: 'services', entityId: params.service, versionsOrActivity: 'versions', version: params.version }, function(serviceVersion) {
+                        resolve(serviceVersion);
+                    }, function(error) {
+                        reject(error);
+                    });
+                })
+            });
             var promise = $q.all(dataLoad);
             $scope.getNextPage = getNextPage;
-            PageLifecycle.loadPage('ServiceContracts', promise, $scope);
+            PageLifecycle.loadPage('ServiceContracts', promise, $scope, function() {
+                Logger.debug("::: is public: {0}", $scope.serviceVersion.publicService);
+                if ($scope.serviceVersion.publicService) {
+                    Logger.debug("::: num plans: {0}", $scope.serviceVersion.plans.length);
+                    if ($scope.serviceVersion.plans.length == 0) {
+                        $scope.isPublicOnly = true;
+                    }
+                }
+            });
         }])
 
 }
