25eb1870db6ea6995501ab84495811f17a8b7fe5
Eric Wittmann <eric.wittmann@gmail.com>
2015-07-07 11:53:02 -0400

    added the publish.sh script

---
+++ publish.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mvn clean install
+cp -rf user-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/user-guide/.
+cp -rf developer-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/developer-guide/.
+cp -rf installation-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/installation-guide/.
+
