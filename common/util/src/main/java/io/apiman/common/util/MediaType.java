e7485b6459135f9364fd0ab1ba448fa08387755e
Marc Savy <marc@rhymewithgravy.com>
2015-08-06 17:47:53 +0100

    Add common media type constants

---
+++ common/util/src/main/java/io/apiman/common/util/MediaType.java
@@ -0,0 +1,6 @@
+package io.apiman.common.util;
+
+public class MediaType {
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String TEXT_PLAIN = "text/plain";
+}
