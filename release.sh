38f60a3083dd02450375a9e32a8c967c64057c6d
Eric Wittmann <eric.wittmann@gmail.com>
2015-08-14 08:50:07 -0400

    release.sh upgrade

--- release.sh
+++ release.sh
@@ -10,8 +10,23 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo "** Current Branch: $BRANCH **"
 echo ""
 
-read -p "Release Version: " RELEASE_VERSION
-read -p "New Development Version: " DEV_VERSION
+RELEASE_VERSION=$1
+DEV_VERSION=$2
+
+if [ "x$RELEASE_VERSION" = "x" ]
+then
+  read -p "Release Version: " RELEASE_VERSION
+fi
+
+if [ "x$DEV_VERSION" = "x" ]
+then
+  read -p "New Development Version: " DEV_VERSION
+fi
+
+echo "######################################"
+echo "Release Version: $RELEASE_VERSION"
+echo "Dev Version: $DEV_VERSION"
+echo "######################################"
 
 mvn versions:set -DnewVersion=$RELEASE_VERSION
 find . -name '*.versionsBackup' -exec rm -f {} \;
