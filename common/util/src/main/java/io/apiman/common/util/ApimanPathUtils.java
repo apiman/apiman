0bd24d2e2d3c6c2456fcfe0266e5595ed8788f1d
Eric Wittmann <eric.wittmann@gmail.com>
2015-08-14 14:50:57 -0400

    fix up version numbers

--- common/util/src/main/java/io/apiman/common/util/ApimanPathUtils.java
+++ common/util/src/main/java/io/apiman/common/util/ApimanPathUtils.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.apiman.common.util;
-
 
 
 public class ApimanPathUtils {
 
-    public static final String X_API_VERSION_HEADER = "X-API-Version";
-    public static final String ACCEPT_HEADER = "Accept";
+    public static final String X_API_VERSION_HEADER = "X-API-Version"; //$NON-NLS-1$
+    public static final String ACCEPT_HEADER = "Accept"; //$NON-NLS-1$
 
     public static final ServiceRequestPathInfo parseServiceRequestPath(String apiVersionHeader, String acceptHeader, String pathInfo) {
         //String pathInfo = request.getPathInfo();
