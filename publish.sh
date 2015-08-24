e40d498dcf7563d3f1812649b79be4607e440a5d
Eric Wittmann <eric.wittmann@gmail.com>
2015-08-24 15:40:13 -0400

    added a new production guide

--- publish.sh
+++ publish.sh
@@ -3,4 +3,5 @@ mvn clean install
 cp -rf user-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/user-guide/.
 cp -rf developer-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/developer-guide/.
 cp -rf installation-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/installation-guide/.
+cp -rf production-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/production-guide/.
 
