b4411d9952c191c1ebdbdfafd60b73bb7c97187b
Marc Savy <marc@rhymewithgravy.com>
2015-09-10 10:58:27 +0100

    Regenerate converters
    Changed annotation names

--- gateway/platforms/vertx3/vertx3/src/main/generated/io/apiman/gateway/platforms/vertx3/io/VertxServiceRequestConverter.java
+++ gateway/platforms/vertx3/vertx3/src/main/generated/io/apiman/gateway/platforms/vertx3/io/VertxServiceRequestConverter.java
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.apiman.gateway.platforms.vertx3.io;
 
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
 
-import java.util.Map.Entry;
-
+/**
+ * Converter for {@link io.apiman.gateway.platforms.vertx3.io.VertxServiceRequest}.
+ *
+ * NOTE: This class has been automatically generated from the {@link io.apiman.gateway.platforms.vertx3.io.VertxServiceRequest} original class using Vert.x codegen.
+ */
 public class VertxServiceRequestConverter {
 
   public static void fromJson(JsonObject json, VertxServiceRequest obj) {
@@ -12,6 +32,25 @@ public class VertxServiceRequestConverter {
     }
     if (json.getValue("destination") instanceof String) {
       obj.setDestination((String)json.getValue("destination"));
+    }
+    if (json.getValue("headers") instanceof JsonObject) {
+      java.util.Map<String, java.lang.String> map = new java.util.LinkedHashMap<>();
+      json.getJsonObject("headers").forEach(entry -> {
+        if (entry.getValue() instanceof String)
+          map.put(entry.getKey(), (String)entry.getValue());
+      });
+      obj.setHeaders(map);
+    }
+    if (json.getValue("queryParams") instanceof JsonObject) {
+      java.util.Map<String, java.lang.String> map = new java.util.LinkedHashMap<>();
+      json.getJsonObject("queryParams").forEach(entry -> {
+        if (entry.getValue() instanceof String)
+          map.put(entry.getKey(), (String)entry.getValue());
+      });
+      obj.setQueryParams(map);
+    }
+    if (json.getValue("rawRequest") instanceof Object) {
+      obj.setRawRequest(json.getValue("rawRequest"));
     }
     if (json.getValue("remoteAddr") instanceof String) {
       obj.setRemoteAddr((String)json.getValue("remoteAddr"));
@@ -31,16 +70,6 @@ public class VertxServiceRequestConverter {
     if (json.getValue("type") instanceof String) {
       obj.setType((String)json.getValue("type"));
     }
-    for (Entry<String, Object> entry : json.getJsonObject("headers").getMap().entrySet()) {
-        if (entry.getValue() instanceof String) {
-            obj.getHeaders().put(entry.getKey(), (String) entry.getValue());
-        }
-    }
-    for (Entry<String, Object> entry : json.getJsonObject("queryParams").getMap().entrySet()) {
-        if (entry.getValue() instanceof String) {
-            obj.getQueryParams().put(entry.getKey(), (String) entry.getValue());
-        }
-    }
   }
 
   public static void toJson(VertxServiceRequest obj, JsonObject json) {
@@ -49,6 +78,19 @@ public class VertxServiceRequestConverter {
     }
     if (obj.getDestination() != null) {
       json.put("destination", obj.getDestination());
+    }
+    if (obj.getHeaders() != null) {
+      JsonObject map = new JsonObject();
+      obj.getHeaders().forEach((key,value) -> map.put(key, value));
+      json.put("headers", map);
+    }
+    if (obj.getQueryParams() != null) {
+      JsonObject map = new JsonObject();
+      obj.getQueryParams().forEach((key,value) -> map.put(key, value));
+      json.put("queryParams", map);
+    }
+    if (obj.getRawRequest() != null) {
+      json.put("rawRequest", obj.getRawRequest());
     }
     if (obj.getRemoteAddr() != null) {
       json.put("remoteAddr", obj.getRemoteAddr());
@@ -65,12 +107,6 @@ public class VertxServiceRequestConverter {
     json.put("transportSecure", obj.isTransportSecure());
     if (obj.getType() != null) {
       json.put("type", obj.getType());
-    }
-    if (obj.getHeaders() != null) {
-      json.put("headers", obj.getHeaders());
-    }
-    if (obj.getQueryParams() != null) {
-        json.put("queryParams", obj.getQueryParams());
     }
   }
 }
