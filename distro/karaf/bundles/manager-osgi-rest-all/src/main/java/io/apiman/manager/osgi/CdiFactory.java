b850550c0e5fee61e2075ac517d54997270a8687
Charles Moulliard <ch007m@gmail.com>
2015-12-09 08:03:11 +0100

    Add new modules

---
+++ distro/karaf/bundles/manager-osgi-rest-all/src/main/java/io/apiman/manager/osgi/CdiFactory.java
@@ -0,0 +1,123 @@
+package io.apiman.manager.osgi;
+
+import io.apiman.common.plugin.Plugin;
+import io.apiman.common.plugin.PluginClassLoader;
+import io.apiman.common.plugin.PluginCoordinates;
+import io.apiman.common.util.ReflectionUtils;
+import io.apiman.manager.api.beans.idm.UserBean;
+import io.apiman.manager.api.core.IApiKeyGenerator;
+import io.apiman.manager.api.core.IMetricsAccessor;
+import io.apiman.manager.api.core.INewUserBootstrapper;
+import io.apiman.manager.api.core.IPluginRegistry;
+import io.apiman.manager.api.core.IServiceCatalog;
+import io.apiman.manager.api.core.IStorage;
+import io.apiman.manager.api.core.IStorageQuery;
+import io.apiman.manager.api.core.UuidApiKeyGenerator;
+import io.apiman.manager.api.core.exceptions.StorageException;
+import io.apiman.manager.api.core.logging.ApimanLogger;
+import io.apiman.manager.api.core.logging.IApimanLogger;
+import io.apiman.manager.api.core.logging.JsonLoggerImpl;
+import io.apiman.manager.api.core.noop.NoOpMetricsAccessor;
+import io.apiman.manager.api.es.ESMetricsAccessor;
+import io.apiman.manager.api.es.EsStorage;
+import io.apiman.manager.api.jpa.JpaStorage;
+import io.apiman.manager.api.security.ISecurityContext;
+import io.apiman.manager.api.security.impl.DefaultSecurityContext;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.client.config.HttpClientConfig.Builder;
+
+import java.lang.reflect.Constructor;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.New;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Create producer methods for CDI beans.
+ */
+@ApplicationScoped public class CdiFactory {
+
+    private static JestClient sStorageESClient;
+    private static EsStorage sESStorage;
+
+    @Produces @ApplicationScoped public static IStorage provideStorage(@New EsStorage esStorage) {
+        IStorage storage = null;
+        storage = initES(esStorage);
+        return storage;
+    }
+
+    @Produces @ApplicationScoped @Named("storage") public static JestClient provideStorageESClient() {
+        if (sStorageESClient == null) {
+            sStorageESClient = createStorageJestClient();
+        }
+        return sStorageESClient;
+    }
+
+    @Produces @ApimanLogger public static IApimanLogger provideLogger(InjectionPoint injectionPoint) {
+        ApimanLogger logger = injectionPoint.getAnnotated().getAnnotation(ApimanLogger.class);
+        Class<?> requestorKlazz = logger.value();
+        return new JsonLoggerImpl().createLogger(requestorKlazz);
+    }
+
+    @Produces @ApplicationScoped
+    public static IStorageQuery provideStorageQuery(@New EsStorage esStorage) {
+        return initES(esStorage);
+    }
+
+
+    /**
+     * Initializes the ES storage (if required).
+     *
+     * @param esStorage
+     */
+    private static EsStorage initES(EsStorage esStorage) {
+        if (sESStorage == null) {
+            sESStorage = esStorage;
+            sESStorage.initialize();
+        }
+        return sESStorage;
+    }
+
+    @Produces @ApplicationScoped public static ISecurityContext provideSecurityContext(
+            @New DefaultSecurityContext defaultSC) {
+        return defaultSC;
+    }
+
+    @Produces @ApplicationScoped public static IApiKeyGenerator provideApiKeyGenerator(
+            @New UuidApiKeyGenerator uuidApiKeyGen) {
+        return uuidApiKeyGen;
+    }
+
+
+    /**
+     * @return create a new test ES client
+     */
+    private static JestClient createStorageJestClient() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("http");
+        builder.append("://"); //$NON-NLS-1$
+        builder.append("localhost");
+        builder.append(":"); //$NON-NLS-1$
+        builder.append("19200");
+        String connectionUrl = builder.toString();
+        JestClientFactory factory = new JestClientFactory();
+        Builder httpConfig = new HttpClientConfig.Builder(connectionUrl).multiThreaded(true);
+        String username = null;
+        String password = null;
+        if (username != null) {
+            httpConfig.defaultCredentials(username, password);
+        }
+        httpConfig.connTimeout(6000);
+        httpConfig.readTimeout(6000);
+        factory.setHttpClientConfig(httpConfig.build());
+        return factory.getObject();
+    }
+
+}
