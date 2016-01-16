9cc5957a7cbae703273b183322781857c79ee3db
Marc Savy <marc@rhymewithgravy.com>
2016-01-16 22:00:27 +0000

    Fix vert.x 3 on 1.1.x branch as snapshot was deleted. Moved to proper released version.

--- gateway/platforms/vertx3/vertx3/src/main/java/io/apiman/gateway/platforms/vertx3/services/package-info.java
+++ gateway/platforms/vertx3/vertx3/src/main/java/io/apiman/gateway/platforms/vertx3/services/package-info.java
@@ -1,5 +1,4 @@
-@GenModule(name = "apiman-vertx-services", groupPackageName="io.apiman.gateway.platforms.vertx3")
+@ModuleGen(name = "apiman-vertx-services", groupPackage="io.apiman.gateway.platforms.vertx3")
 package io.apiman.gateway.platforms.vertx3.services;
 
-import io.vertx.codegen.annotations.GenModule;
-
+import io.vertx.codegen.annotations.ModuleGen;
