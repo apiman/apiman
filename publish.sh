ab5e86af3323aca3d9581dbb4e0d5270cbaf063a
Eric Wittmann <eric.wittmann@gmail.com>
2016-01-26 08:50:14 -0500

    updated some documentation based on errors found by community members

--- publish.sh
+++ publish.sh
@@ -5,3 +5,9 @@ cp -rf developer-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/l
 cp -rf installation-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/installation-guide/.
 cp -rf production-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/production-guide/.
 cp -rf production-guide/en-US/images/* ../apiman.github.io/latest/images/images
+
+cp developer-guide/target/docbook/publish/en-US/pdf/apiman-developer-guide.pdf ../apiman.github.io/latest/developer-guide/.
+cp installation-guide/target/docbook/publish/en-US/pdf/apiman-installation-guide.pdf ../apiman.github.io/latest/installation-guide/.
+cp production-guide/target/docbook/publish/en-US/pdf/apiman-production-guide.pdf ../apiman.github.io/latest/production-guide/.
+cp user-guide/target/docbook/publish/en-US/pdf/apiman-user-guide.pdf ../apiman.github.io/latest/user-guide/.
+
