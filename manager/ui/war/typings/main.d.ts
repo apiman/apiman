78adfcac058c22c5774cb228704cd53516dc0356
kahboom <rachelyordan@gmail.com>
2016-02-23 15:15:38 -0500

    Add typings dir, update gitignore, rm setup step from package.json

---
+++ manager/ui/war/typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main/ambient/angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
+/// <reference path="main/ambient/angular/angular.d.ts" />
+/// <reference path="main/ambient/jquery/jquery.d.ts" />
+/// <reference path="main/ambient/lodash/lodash.d.ts" />
