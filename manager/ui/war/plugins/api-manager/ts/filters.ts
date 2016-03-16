279da8328a56d18891acdd2742f22a30dbdf3cca
kahboom <rachelyordan@gmail.com>
2016-03-16 14:04:33 -0400

    re #1025 - API Catalog: Ability to search for and filter with multiple tags
    Disable custom tag label

--- manager/ui/war/plugins/api-manager/ts/filters.ts
+++ manager/ui/war/plugins/api-manager/ts/filters.ts
@@ -45,5 +45,56 @@ module ApimanFilters {
         };
     });
 
+    export var propsFilter = _module.filter('propsFilter', function() {
+        return function(items, props) {
+            var out = [];
+
+            if (angular.isArray(items)) {
+                items.forEach(function(item) {
+                    var itemMatches = false;
+
+                    var keys = Object.keys(props);
+                    for (var i = 0; i < keys.length; i++) {
+                        var prop = keys[i];
+                        var text = props[prop].toLowerCase();
+                        if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+                            itemMatches = true;
+                            break;
+                        }
+                    }
+
+                    if (itemMatches) {
+                        out.push(item);
+                    }
+                });
+            } else {
+                // Let the output be the input untouched
+                out = items;
+            }
+
+            return out;
+        };
+    });
+
+    export var selectedTags = _module.filter('selectedTags', ['Logger', function(Logger) {
+        return function(array, selectedTags) {
+            if(array && selectedTags.length >= 1) {
+                return array.filter(function(item) {
+                    var itemTags = [];
+
+                    _.map(selectedTags, function(tag) {
+                        if(_.includes(item.tags, tag)) {
+                            itemTags.push(tag);
+                        }
+                    });
+
+                    return _.difference(selectedTags, itemTags).length === 0;
+                });
+            } else {
+                return array;
+            }
+        };
+    }]);
+
 }
 
