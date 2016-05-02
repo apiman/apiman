84c7b73b5825754513a2e24b3e864df90a7f4340
Eric Wittmann <eric.wittmann@gmail.com>
2016-05-02 08:55:55 -0400

    made the crash course document a separage guide

--- publish.sh
+++ publish.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 mvn clean install
+cp -rf crash-course/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/crash-course/.
+cp -rf crash-course/en-US/images/* ../apiman.github.io/latest/images/images
 cp -rf user-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/user-guide/.
 cp -rf developer-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/developer-guide/.
 cp -rf installation-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/installation-guide/.
 cp -rf production-guide/target/docbook/publish/en-US/html/* ../apiman.github.io/latest/production-guide/.
 cp -rf production-guide/en-US/images/* ../apiman.github.io/latest/images/images
 
+cp crash-course/target/docbook/publish/en-US/pdf/apiman-crash-course.pdf ../apiman.github.io/latest/crash-course/.
 cp developer-guide/target/docbook/publish/en-US/pdf/apiman-developer-guide.pdf ../apiman.github.io/latest/developer-guide/.
 cp installation-guide/target/docbook/publish/en-US/pdf/apiman-installation-guide.pdf ../apiman.github.io/latest/installation-guide/.
 cp production-guide/target/docbook/publish/en-US/pdf/apiman-production-guide.pdf ../apiman.github.io/latest/production-guide/.
