b2e1e287e05cf8e7d7f826ee06f915e1b5ac8b85
Marc Savy <marc@rhymewithgravy.com>
2017-02-03 15:15:01 +0000

    fix(gateway-platforms-vertx3) Fix potential NPE when config is missing.

--- gateway/platforms/vertx3/vertx3/src/main/java/io/apiman/gateway/platforms/vertx3/io/package-info.java
+++
@@ -1,5 +0,0 @@
-@ModuleGen(name = "apiman-vertx-services", groupPackage="io.apiman.gateway.platforms.vertx3")
-package io.apiman.gateway.platforms.vertx3.io;
-
-import io.vertx.codegen.annotations.ModuleGen;
-
