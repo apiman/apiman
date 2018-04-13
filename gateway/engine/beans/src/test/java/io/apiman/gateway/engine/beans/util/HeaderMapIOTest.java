6f886de83d5f7bdfaaa5417a0a2b29222f487740
Marc Savy <marc@rhymewithgravy.com>
2018-04-13 22:40:30 +0100

    test(gateway-engine-beans): Test HeaderMap serializer and deserializer.
    Signed-off-by: Marc Savy <marc@rhymewithgravy.com>

--- gateway/engine/beans/src/test/java/io/apiman/gateway/engine/beans/util/HeaderMapIOTest.java
+++ gateway/engine/beans/src/test/java/io/apiman/gateway/engine/beans/util/HeaderMapIOTest.java
@@ -16,9 +16,14 @@
 
 package io.apiman.gateway.engine.beans.util;
 
+import java.io.IOException;
+
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -34,12 +39,12 @@ public class HeaderMapIOTest {
     public void testJsonMarshalling_SingleProperty() throws Exception {
         HeaderMap original = new HeaderMap();
         original.add("Header-1", "Value-1");
-        
+
         ObjectMapper mapper = new ObjectMapper();
         String serializedHeaderMap = mapper.writer().writeValueAsString(original);
-        
+
         HeaderMap deserializedMap = mapper.reader(HeaderMap.class).readValue(serializedHeaderMap);
-        
+
         Assert.assertNotNull(deserializedMap.get("Header-1"));
         Assert.assertEquals("Value-1", deserializedMap.get("Header-1"));
     }
@@ -54,13 +59,13 @@ public class HeaderMapIOTest {
         original.add("Header-2", "Value-2");
         original.add("Header-3", "Value-3");
         original.add("Header-4", "Value-4");
-        
+
         ObjectMapper mapper = new ObjectMapper();
         String serializedHeaderMap = mapper.writer().writeValueAsString(original);
-        
+
         HeaderMap deserializedMap = mapper.reader(HeaderMap.class).readValue(serializedHeaderMap);
         System.out.println(deserializedMap);
-        
+
         Assert.assertNotNull(deserializedMap.get("Header-1"));
         Assert.assertEquals("Value-1", deserializedMap.get("Header-1"));
         Assert.assertNotNull(deserializedMap.get("Header-2"));
@@ -69,6 +74,31 @@ public class HeaderMapIOTest {
         Assert.assertEquals("Value-3", deserializedMap.get("Header-3"));
         Assert.assertNotNull(deserializedMap.get("Header-4"));
         Assert.assertEquals("Value-4", deserializedMap.get("Header-4"));
+    }
+
+    @Test
+    public void testJsonMarshalling_DuplicateValues() throws JsonParseException, JsonMappingException, IOException {
+        HeaderMap map = new HeaderMap();
+        map.add("foo", "val");
+        map.add("foo", "val2");
+        map.add("weeble", "wobble");
+        map.add("savy", "marc");
+        map.add("savy", "alain");
+        map.add("savy", "yvon");
+        map.add("Single", "thing");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String actualWritten = mapper.writeValueAsString(map);
+        Assert.assertEquals("{\"foo\":[\"val2\",\"val\"],\"savy\":[\"yvon\",\"alain\",\"marc\"],\"Single\":\"thing\",\"weeble\":\"wobble\"}",
+                actualWritten);
+
+        HeaderMap deserializedMap = mapper.readValue(actualWritten, HeaderMap.class);
+
+       Assert.assertThat(deserializedMap.getAll("foo"), Matchers.hasItems("val2", "val"));
+       Assert.assertThat(deserializedMap.getAll("savy"), Matchers.hasItems("marc", "alain", "yvon"));
+       Assert.assertThat(deserializedMap.getAll("weeble"), Matchers.hasItems("wobble"));
+       Assert.assertEquals(deserializedMap.get("Single"), "thing");
+       Assert.assertEquals(deserializedMap.get("savy"), "yvon");
     }
 
 }
