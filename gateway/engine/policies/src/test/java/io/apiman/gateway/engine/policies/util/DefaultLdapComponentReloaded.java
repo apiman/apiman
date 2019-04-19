8c049c2a2f2e4a69bbb6125686a15edd26f29c21
jhauray <42616019+jhauray@users.noreply.github.com>
2019-04-19 19:35:15 +0200

    apiman-722 : BasicAuth, LDAPS - Fix Error "SERVER_DOWN, java.net.SocketException: Connection reset" in SSL. (#728)
    * BasicAuth, LDAPS - Fix Error "SERVER_DOWN, java.net.SocketException: Connection reset" in SSL. + Add a unit test class : BasicAuthLDAPSTest.
    
    * BasicAuthLDAPSTest : Use independante directory partition
    
    * BasicAuthLDAPSTest : Force Keystore type to JKS, check Keystore saving and output truststore system properties.
    
    * BasicAuthLDAPSTest : Enable -Djavax.net.debug=ssl:handshake for troobleshooting SSL error.
    
    * fix BasicAuthLDAPSTest error, javax.net.ssl.SSLException: Received fatal alert: certificate_unknown
    
    * Code formatting review.

---
+++ gateway/engine/policies/src/test/java/io/apiman/gateway/engine/policies/util/DefaultLdapComponentReloaded.java
@@ -0,0 +1,36 @@
+package io.apiman.gateway.engine.policies.util;
+
+import com.unboundid.util.ssl.SSLUtil;
+import io.apiman.gateway.engine.impl.DefaultLdapComponent;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+/**
+ * For tests only
+ * Used to force reloading static SSLContext and SSLSocketFactory, without modifying DefaultLdapComponent
+ *
+ * @author jhauray
+ */
+public class DefaultLdapComponentReloaded extends DefaultLdapComponent {
+
+    public DefaultLdapComponentReloaded() throws GeneralSecurityException {
+
+        // Force reloading static SSLSocketFactory to accept truststore changes.
+        TrustManagerFactory tmFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmFactory.init((KeyStore) null);
+
+        X509TrustManager trustManager = null;
+        for (TrustManager tm : tmFactory.getTrustManagers()) {
+            if (tm instanceof X509TrustManager) {
+                trustManager = (X509TrustManager) tm;
+                break;
+            }
+        }
+
+        DEFAULT_SOCKET_FACTORY = new SSLUtil(trustManager).createSSLSocketFactory();
+    }
+}
