028bdae43562fbcc6306d8d044fe8ec61c8c8b36
Benjamin Kihm <benjamin.kihm@scheer-group.com>
2019-10-28 15:37:38 +0100

    keycloak logout

--- src/app/pas-header/pas-header.component.ts
+++ src/app/pas-header/pas-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-pas-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private keycloak: KeycloakService) { }
 
   ngOnInit() {
+  }
+
+  public logout() {
+    localStorage.setItem('apiman_keycloak_token', '');
+    localStorage.setItem('apiman_keycloak_refresh_token', '');
+    this.keycloak.getKeycloakInstance().logout();
   }
 
 }
