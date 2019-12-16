67e7e167d9c5e9fbb8b2973ab7cec4d2b9beb0f0
Florian Volk <florian.volk@scheer-group.com>
2019-12-16 11:11:27 +0100

    GW-276: Remove docker image after pipeline

--- ci/publish-images.sh
+++ ci/publish-images.sh
@@ -5,6 +5,7 @@
 if [[ $2 = "release" ]]; then
     docker tag api-mgmt/devportal:$1 gitlab.scheer-group.com:8080/api-mgmt/devportal:$1
     docker push gitlab.scheer-group.com:8080/api-mgmt/devportal:$1
+    docker image rm -f gitlab.scheer-group.com:8080/api-mgmt/devportal:$1
 else
     docker tag api-mgmt/devportal:$1 gitlab.scheer-group.com:8080/api-mgmt/devportal:latest
     docker push gitlab.scheer-group.com:8080/api-mgmt/devportal:latest
