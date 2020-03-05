e3dc0db5c8afd41d99cb910265ab9e83f3b5b466
Eric Wittmann <eric.wittmann@gmail.com>
2020-03-05 07:21:31 -0500

    Added orgs pagination to search (#810)
    * Replacement of #806. Added pagination
    
    * Replacement of #806. Added pagination
    
    * Updated version and date of sample config file.

--- manager/ui/war/plugins/api-manager/ts/consumer/consumer-orgs.ts
+++ manager/ui/war/plugins/api-manager/ts/consumer/consumer-orgs.ts
@@ -5,22 +5,36 @@ module Apiman {
         ['$q', '$location', '$scope', 'ApimanSvcs', 'PageLifecycle', 'Logger', 'CurrentUser',
         ($q, $location, $scope, ApimanSvcs, PageLifecycle, Logger, CurrentUser) => {
             var params = $location.search();
+            
             if (params.q) {
                 $scope.orgName = params.q;
             }
             
-            $scope.searchOrg = function(value) {
-                $location.search('q', value);
+            $scope.searchOrg = function(value, currentPage, pageSize) {
+                $location.search({
+                    'q': value,
+                    'cp': currentPage,
+                    'ps': pageSize
+                });
             };
             
             var pageData = {
                 orgs: $q(function(resolve, reject) {
-                    if (params.q) {
+                    if (params.q && params.cp && params.ps) {
                         var body:any = {};
                         body.filters = [];
+                        
+                        body.page = params.cp
+                        body.pageSize = params.ps
+
                         body.filters.push( {"name": "name", "value": "*" + params.q + "*", "operator": "like"});
                         var searchStr = angular.toJson(body);
                         ApimanSvcs.save({ entityType: 'search', secondaryType: 'organizations' }, searchStr, function(result) { 
+            
+                            $scope.resultCount = result.totalSize;
+                            $scope.currentPage = params.cp;
+                            $scope.pageSize = params.ps
+            
                             resolve(result.beans);
                         }, reject);
                     } else {
