6dccfb23964e97fe7ab771e199f3d724ea36a3de
Florian Volk <florian.volk@scheer-group.com>
2020-03-25 12:52:03 +0100

    Some little improvements (Paging, HTML, Swagger) (#826)
    * Some improvements after merge for paging and REST
    
    * Reformat HTML
    
    * HTML Refactoring:
    - do not close meta tags
    - do not close input tags

--- manager/ui/war/plugins/api-manager/ts/consumer/consumer-orgs.ts
+++ manager/ui/war/plugins/api-manager/ts/consumer/consumer-orgs.ts
@@ -5,11 +5,11 @@ module Apiman {
         ['$q', '$location', '$scope', 'ApimanSvcs', 'PageLifecycle', 'Logger', 'CurrentUser',
         ($q, $location, $scope, ApimanSvcs, PageLifecycle, Logger, CurrentUser) => {
             var params = $location.search();
-            
+
             if (params.q) {
                 $scope.orgName = params.q;
             }
-            
+
             $scope.searchOrg = function(value, currentPage, pageSize) {
                 $location.search({
                     'q': value,
@@ -17,24 +17,24 @@ module Apiman {
                     'ps': pageSize
                 });
             };
-            
+
             var pageData = {
                 orgs: $q(function(resolve, reject) {
                     if (params.q && params.cp && params.ps) {
                         var body:any = {};
                         body.filters = [];
-                        
-                        body.page = params.cp
-                        body.pageSize = params.ps
+
+                        body.page = params.cp;
+                        body.pageSize = params.ps;
 
                         body.filters.push( {"name": "name", "value": "*" + params.q + "*", "operator": "like"});
                         var searchStr = angular.toJson(body);
                         ApimanSvcs.save({ entityType: 'search', secondaryType: 'organizations' }, searchStr, function(result) { 
-            
+
                             $scope.resultCount = result.totalSize;
                             $scope.currentPage = params.cp;
-                            $scope.pageSize = params.ps
-            
+                            $scope.pageSize = params.ps;
+
                             resolve(result.beans);
                         }, reject);
                     } else {
@@ -42,14 +42,21 @@ module Apiman {
                     }
                 })
             };
-            
+
+            function loadFirstPage() {
+                if ($scope.orgs.length == 0) {
+                    $scope.searchOrg('*', 1, 10);
+                }
+            }
+
             PageLifecycle.loadPage('ConsumerOrgs', undefined, pageData, $scope, function() {
                 PageLifecycle.setPageTitle('consumer-orgs');
+                loadFirstPage();
                 $scope.$applyAsync(function() {
                     angular.forEach($scope.orgs, function(org) {
                         org.isMember = CurrentUser.isMember(org.id);
                     });
-                    $('#apiman-search').focus();
+                    $('#apiman-search').val('').focus();
                 });
             });
         }]);
