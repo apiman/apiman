66d1a8f98a029a9f03fb5533508047f13bf149d8
Florian Volk <florian.volk@scheer-group.com>
2020-04-20 11:26:59 +0200

    Add intellij config

--- src/app/components/admin/edit-developer/keycloak-user-impl.spec.ts
+++
@@ -1,7 +0,0 @@
-import { KeycloakUserImpl } from './keycloak-user-impl';
-
-describe('KeycloakUserImpl', () => {
-  it('should create an instance', () => {
-    expect(new KeycloakUserImpl()).toBeTruthy();
-  });
-});
--- src/app/components/admin/edit-developer/keycloak-user-impl.ts
+++
@@ -1,9 +0,0 @@
-import {KeycloakUser} from '../../../services/api-data.service';
-
-export class KeycloakUserImpl implements KeycloakUser {
-  email: string;
-  firstName: string;
-  lastName: string;
-  password: string;
-  username: string;
-}
