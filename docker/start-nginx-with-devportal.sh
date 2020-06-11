9fa6f9acbfe287503dee9695f40ebf33ac090e41
Florian Volk <florian.volk@scheer-group.com>
2020-06-11 12:55:41 +0200

    BPAAS-6301: Make devportal one-docker ready

--- docker/start-nginx-with-devportal.sh
+++ docker/start-nginx-with-devportal.sh
@@ -8,4 +8,15 @@ do
   envsubst "\$KEYCLOAK_REALM \$API_MGMT_UI_REST_URL \$KEYCLOAK_AUTH_URL"  < "$mainFileName" > main.tmp
   mv main.tmp "${mainFileName}"
 done
-nginx -g 'daemon off;'
+
+# Check if a prefix is set
+if [ -z "$SYSTEM_PREFIX" ]
+then
+      # If no prefix start nginx
+      nginx -g 'daemon off;'
+else
+      # If a prefix is set we will replace the base path in index.html before starting tomcat
+      # Replace '/' with e.g. '/pas/devportal/'
+      sed -i "s/'\/'/'\/$SYSTEM_PREFIX\/devportal\/'/g" "/usr/share/nginx/html/index.html"
+      nginx -g 'daemon off;'
+fi
