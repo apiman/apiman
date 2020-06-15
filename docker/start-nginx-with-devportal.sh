f8b9500bd6608e8fb38607628ed06a5a0d192cc3
Benjamin Kihm <benjamin.kihm@scheer-group.com>
2020-06-15 15:00:57 +0200

    GW-385: Change base path and startup script

--- docker/start-nginx-with-devportal.sh
+++ docker/start-nginx-with-devportal.sh
@@ -9,14 +9,12 @@ do
   mv main.tmp "${mainFileName}"
 done
 
-# Check if a prefix is set
-if [ -z "$SYSTEM_PREFIX" ]
+# Check if a prefix exists
+if [ -n "$SYSTEM_PREFIX" ]
 then
-      # If no prefix start nginx
-      nginx -g 'daemon off;'
-else
-      # If a prefix is set we will replace the base path in index.html before starting tomcat
-      # Replace '/' with e.g. '/pas/devportal/'
-      sed -i "s/'\/'/'\/$SYSTEM_PREFIX\/devportal\/'/g" "/usr/share/nginx/html/index.html"
-      nginx -g 'daemon off;'
+  # If a prefix is set we will replace the base path in index.html before starting the application
+  # Replace "/" with e.g. "/pas/devportal/"
+  sed -i "s/\"\/\"/\"\/$SYSTEM_PREFIX\/devportal\/\"/g" "/usr/share/nginx/html/index.html"
 fi
+# start nginx
+nginx -g 'daemon off;'
