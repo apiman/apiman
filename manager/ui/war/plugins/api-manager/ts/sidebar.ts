84c6a84eae1b1e4f9ff39870a99321add0096369
Estevao Souza <tevocsouza@gmail.com>
2020-07-13 09:36:58 -0300

    added fixed sidebar navigation

---
+++ manager/ui/war/plugins/api-manager/ts/sidebar.ts
@@ -0,0 +1,10 @@
+/// <reference path="apimanPlugin.ts"/>
+module Apiman {
+
+    export var SidebarController = _module.controller("Apiman.SidebarController",
+        ['$scope', 'Logger', 'Configuration', ($scope, Logger, Configuration) => {
+            
+            
+        }]);
+
+}
