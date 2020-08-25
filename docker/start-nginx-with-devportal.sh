16fcd40bf437faa4868af50f3ade5bd59a7f6d5d
Benjamin Kihm <benjamin.kihm@scheer-group.com>
2020-08-25 10:40:43 +0200

    Change settings for local development

--- docker/start-nginx-with-devportal.sh
+++ docker/start-nginx-with-devportal.sh
@@ -13,8 +13,8 @@ done
 if [ -n "$SYSTEM_PREFIX" ]
 then
   # If a prefix is set we will replace the base path in index.html before starting the application
-  # Replace "/" with e.g. "/pas/devportal/"
-  sed -i "s/\"\/\"/\"\/$SYSTEM_PREFIX\/devportal\/\"/g" "/usr/share/nginx/html/index.html"
+  # Replace base tag with base tag containing system prefix src e.g. "/pas/devportal/"
+  sed -i "s/<base.*>/<base src=\"$SYSTEM_PREFIX\/devportal\/\">/g" "/usr/share/nginx/html/index.html"
 fi
 # start nginx
 nginx -g 'daemon off;'
