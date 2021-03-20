075e4f7dac2bb0fcda1ba5b3f49b9d238dd21d20
Marc Savy <marc@rhymewithgravy.com>
2021-03-20 16:31:32 +0000

    Work around bootstrap-select + patternfly issue

--- manager/ui/war/entry.js
+++ manager/ui/war/entry.js
@@ -5,12 +5,9 @@
 
 global.jQuery = global.$ = require('jquery/dist/jquery.js');
 var bootstrap = require('bootstrap/dist/js/bootstrap.js');
-var bootstrapSelect = require('bootstrap-select/dist/js/bootstrap-select.js');
 var jsonEditor = require('json-editor');
 var patternfly = require('patternfly/dist/js/patternfly.js');
 var angular = require('angular');
-var uiBootstrap = require('angular-ui-bootstrap/ui-bootstrap.js');
-var uiBootstrapTpls = require('angular-ui-bootstrap/ui-bootstrap-tpls.js');
 var uiSelect = require('ui-select/dist/select.js');
 var ngClipboard = require('angular-clipboard');
 var ngResource = require('angular-resource');
@@ -28,7 +25,9 @@ var ngSchemaForm = require('angular-schema-form');
 global.SwaggerUIBundle = require('swagger-ui-dist/swagger-ui-bundle.js');
 global.SwaggerClient = require('swagger-client');
 
-
 var sugar = require('sugar');
 var ngFileUpload = require('ng-file-upload');
 global.moment = require('moment/min/moment.min.js');
+var bootstrapSelect = require('bootstrap-select/dist/js/bootstrap-select.js');
+var uiBootstrap = require('angular-ui-bootstrap/ui-bootstrap.js');
+var uiBootstrapTpls = require('angular-ui-bootstrap/ui-bootstrap-tpls.js');
