a444a3d2141e1d5099ecb58eb02ddb2dba78425e
Marc Savy <marc@rhymewithgravy.com>
2021-05-07 13:53:27 +0100

    wip

--- manager/api/rest/src/main/java/io/apiman/manager/api/rest/ISystemResource.java
+++ manager/api/rest/src/main/java/io/apiman/manager/api/rest/ISystemResource.java
@@ -19,7 +19,13 @@ package io.apiman.manager.api.rest;
 import io.apiman.manager.api.beans.system.SystemStatusBean;
 import io.swagger.annotations.Api;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -36,6 +42,7 @@ public interface ISystemResource {
      * This endpoint simply returns the status of the apiman system.  This is
      * a useful endpoint to use when testing a client's connection to the apiman
      * API Manager REST services.
+     *
      * @summary Get System Status
      * @statuscode 200 On success.
      * @return System status information.
@@ -43,7 +50,7 @@ public interface ISystemResource {
     @GET
     @Path("status")
     @Produces(MediaType.APPLICATION_JSON)
-    public SystemStatusBean getStatus();
+    SystemStatusBean getStatus();
     
     /**
      * Use this endpoint to export data from the API Manager to a file.  All data
@@ -51,6 +58,7 @@ public interface ISystemResource {
      * The resulting file should be suitable for importing into some other instance
      * of the apiman API Manager.  This is useful for upgrades, migrations between
      * environments, and backups.
+     *
      * @summary Export Data
      * @servicetag admin
      * @statuscode 200 On successful export
@@ -60,14 +68,23 @@ public interface ISystemResource {
     @GET
     @Path("export")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response exportData(@QueryParam("download") String download) throws NotAuthorizedException;
+    Response exportData(@QueryParam("download") String download) throws NotAuthorizedException;
+
     // "Internal" method - called by the download resource.
-    public Response exportData();
-    
+    Response exportData();
+
+    /**
+     * Import data into Apiman.
+     *
+     * @summary import data into Apiman that was previously exported
+     * @servicetag admin
+     * @statuscode 200 On successful import
+     * @throws NotAuthorizedException when the user attempts to do or see something that they are not authorized (do not have permission) to
+     * @return A logged summary of the importation process
+     */
     @POST
     @Path("import")
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response importData() throws NotAuthorizedException;
-    
+    Response importData() throws NotAuthorizedException;
 }
