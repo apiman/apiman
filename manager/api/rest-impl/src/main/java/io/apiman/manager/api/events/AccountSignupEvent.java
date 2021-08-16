9ff9543fb7918de0cfaa5e7151754d84b239ebe1
Marc Savy <marc@rhymewithgravy.com>
2021-08-16 12:49:55 +0100

    wip

--- manager/api/rest-impl/src/main/java/io/apiman/manager/api/events/AccountSignupEvent.java
+++ manager/api/rest-impl/src/main/java/io/apiman/manager/api/events/AccountSignupEvent.java
@@ -3,6 +3,11 @@ package io.apiman.manager.api.events;
 /**
  * @author Marc Savy {@literal <marc@blackparrotlabs.io>}
  */
-public class AccountSignupEvent {
-    String userId;
+public class AccountSignupEvent extends VersionedApimanEvent {
+    private String userId;
+    // What details in here, hmm!
+
+    public AccountSignupEvent(ApimanEventHeaders headers) {
+        super(headers);
+    }
 }
--- manager/api/rest-impl/src/main/java/io/apiman/manager/api/events/VersionedApimanEvent.java
+++ manager/api/rest-impl/src/main/java/io/apiman/manager/api/events/VersionedApimanEvent.java
@@ -1,23 +1,21 @@
 package io.apiman.manager.api.events;
 
-import java.net.URI;
-import java.time.OffsetDateTime;
-
 /**
- * Uses cloud events-style fields
+ * All Apiman events should extend this class.
+ *
+ * <p>Contains headers segment for various metadata that is useful to the event but does not pertain to the
+ * semantics of the event itself (such as version, timestamps, etc.). This is mostly aligned with CloudEvents.
  *
  * @author Marc Savy {@literal <marc@blackparrotlabs.io>}
  */
 public abstract class VersionedApimanEvent {
-    String id;
-    String eventVersion;
-    URI source;
-    String type;
-    String subject;
-    OffsetDateTime time;
-    ApimanEventData data; // Actual event
+    protected final ApimanEventHeaders headers;
 
-    public VersionedApimanEvent() {
+    public VersionedApimanEvent(ApimanEventHeaders headers) {
+        this.headers = headers;
+    }
 
+    public ApimanEventHeaders getHeaders() {
+        return headers;
     }
 }
