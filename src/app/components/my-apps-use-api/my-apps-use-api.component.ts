321245cd3a78d87ce547f1bb1e05e1fd861d98ba
Florian Volk <florian.volk@scheer-group.com>
2021-10-04 15:18:15 +0200

    fix: reorder config file and add translation strings

--- src/app/components/my-apps-use-api/my-apps-use-api.component.ts
+++ src/app/components/my-apps-use-api/my-apps-use-api.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend/backend.service';
 import { SnackbarService } from '../../services/snackbar/snackbar.service';
-import {ISignUpInfo} from "../../interfaces/ISignUpInfo";
-import {SignUpService} from "../../services/sign-up/sign-up.service";
-import {IContractExt} from "../../interfaces/IContractExt";
+import { ISignUpInfo } from '../../interfaces/ISignUpInfo';
+import { SignUpService } from '../../services/sign-up/sign-up.service';
+import { IContractExt } from '../../interfaces/IContractExt';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-apps-use-api',
@@ -14,17 +15,14 @@ export class MyAppsUseApiComponent implements OnInit, OnChanges {
   @Input() contract?: IContractExt;
   newContractDetails: ISignUpInfo;
 
-  mockText =
-    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et';
-
   apiKey = '';
   endpoint = '';
   oAuthServerUrl = '';
   oAuthClientSecret = '';
 
   previewText = {
-    apiKey: 'You will be provided with an API-Key after sign-up',
-    endpoint: 'You will be provided with an endpoint after sign-up',
+    apiKey: <string>this.translator.instant('WIZARD.API_KEY_PREVIEW'),
+    endpoint: <string>this.translator.instant('WIZARD.API_ENDPOINT_PREVIEW'),
     oAuthServerUrl: 'http://example.org/auth/realms/example/foo',
     oAuthClientSecret: '73b985d1-98b8-4d80-a89d-9dbee3b21a17',
   };
@@ -35,6 +33,7 @@ export class MyAppsUseApiComponent implements OnInit, OnChanges {
     private backend: BackendService,
     private snackbar: SnackbarService,
     private signUpService: SignUpService,
+    private translator: TranslateService
   ) {
     this.newContractDetails = this.signUpService.getSignUpInfo();
   }
@@ -50,7 +49,7 @@ export class MyAppsUseApiComponent implements OnInit, OnChanges {
   private initProperties() {
     if (this.contract) {
       this.disableButtons = false;
-      this.apiKey = this.contract!.client.apikey;
+      this.apiKey = this.contract.client.apikey;
       this.endpoint = this.contract.managedEndpoint;
     } else {
       this.disableButtons = true;
