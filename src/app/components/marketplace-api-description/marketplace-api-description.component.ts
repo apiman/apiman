b3916dc721bdf93a39f1ad288d998b0b9e43cea6
Florian Volk <florian.volk@scheer-group.com>
2021-10-12 10:44:33 +0200

    fix: policy header, create app, api short description

--- src/app/components/marketplace-api-description/marketplace-api-description.component.ts
+++ src/app/components/marketplace-api-description/marketplace-api-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {IApiVersionExt} from "../../interfaces/IApiVersionExt";
+import {IApiVersionExt} from '../../interfaces/IApiVersionExt';
 
 @Component({
   selector: 'app-marketplace-api-description',
@@ -11,8 +11,17 @@ export class MarketplaceApiDescriptionComponent implements OnInit {
   @Input() isLatest!: boolean;
 
   features: string[] = ['fast', 'free', 'fancy features'];
+  shortDescription = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getShortDescription();
+  }
+
+  private getShortDescription() {
+    if (this.api.api.description) {
+      this.shortDescription = this.api.api.description.substring(0, 240);
+    }
+  }
 }
