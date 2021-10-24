5eba830e010494056362890e1986b22a2c0a334b
Marc Savy <marc@rhymewithgravy.com>
2021-10-25 00:26:38 +0100

    Fix TX error

--- manager/api/rest-impl/src/main/java/io/apiman/manager/api/rest/impl/SearchResourceImpl.java
+++ manager/api/rest-impl/src/main/java/io/apiman/manager/api/rest/impl/SearchResourceImpl.java
@@ -43,6 +43,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 /**
  * Implementation of the Search API.
@@ -50,6 +51,7 @@ import javax.inject.Inject;
  * @author eric.wittmann@redhat.com
  */
 @ApplicationScoped
+@Transactional
 public class SearchResourceImpl implements ISearchResource {
 
     private IApiCatalog apiCatalog;
