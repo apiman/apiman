8ba13b68453ee0fd66f7fb4f91c155acb23bfc73
Marc Savy <marc@rhymewithgravy.com>
2021-10-29 10:39:11 +0100

    build(ui): temporarily disable source maps for production
    Due to bloating of the production builds. We may consider some more optimised ways of adding these 
    back in future as they are very useful for production reporting and debugging.

--- manager/ui/war/webpack.config.js
+++ manager/ui/war/webpack.config.js
@@ -1,68 +1,68 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './plugins/api-manager/ts/apimanPlugin.ts'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 2772,
-    static: './dist',
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Apiman',
-      template: './index.html'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "./apiman", to: "apiman" },
-        { 
-          from: "./plugins/api-manager/**/*.{html,include}",
-          to({ context, absoluteFilename }) {
-            return `${path.relative("./", absoluteFilename)}`;
-          },
-       }
-      ],
-    }),
-    new BundleAnalyzerPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /'\.(html|include)$/i,
-        type: 'asset/resource'
-      }
+module.exports = (env, argv) => { 
+  return {
+    mode: 'development',
+    entry: {
+      main: './plugins/api-manager/ts/apimanPlugin.ts'
+    },
+    devtool: argv.mode === 'development' ? 'source-map' : false,
+    devServer: {
+      port: 2772,
+      static: './dist',
+      historyApiFallback: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Apiman',
+        template: './index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "./apiman", to: "apiman" },
+          { 
+            from: "./plugins/api-manager/**/*.{html,include}",
+            to({ context, absoluteFilename }) {
+              return `${path.relative("./", absoluteFilename)}`;
+            },
+        }
+        ],
+      })
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'apiman-manager-ui.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /'\.(html|include)$/i,
+          type: 'asset/resource'
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'apiman-manager-ui.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    }
   }
 };
