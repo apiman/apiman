f17a7f6bb8be7f6789b750799a989c3057d6b978
Marc Savy <marc@rhymewithgravy.com>
2021-11-12 16:28:36 +0000

    build: backport fastbuild.sh script from 3.x
    Script to build the project quickly in parallel without tests, Javadoc, etc.

---
+++ fastbuild.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -o nounset
+set -o errtrace
+set -e
+set -x
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export MVN=
+
+if ! command -v mvnd &> /dev/null
+then
+    echo "*** mvnd (maven daemon) could not be found. Using Maven Wrapper. https://github.com/mvndaemon/mvnd#how-to-install-mvnd ***"
+    MVN="$SCRIPT_DIR/mvnw -T1C "
+else
+    MVN=mvnd
+fi
+
+echo "Fast build skips tests, Javadoc, and various other non-essentials for quickly rebuilding."
+
+cd $SCRIPT_DIR
+cd parent
+$MVN clean install
+cd $SCRIPT_DIR
+$MVN clean install -DskipTests -Dmaven.javadoc.skip -nsu -Pinstall-all-wildfly -Pdocker -Dmaven.wagon.http.retryHandler.count=3 -Dmaven.wagon.httpconnectionManager.ttlSeconds=25
+
+echo "Done"
+
+
+
