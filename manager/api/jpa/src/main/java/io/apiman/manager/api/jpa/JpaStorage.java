9713066fbcdd5d44292582a36578c195b57f3c68
Marc Savy <marc@rhymewithgravy.com>
2021-11-24 09:42:42 +0000

    fix(manager-api-jpa): orgId must be part of #getApiDefinition select to ensure uniqueness
    If orgId not included multiple schemas may be returned if there are mirrored setups in multiple
    orgs. This will cause errors during export, for example.
    
    Fixes #1642

--- manager/api/jpa/src/main/java/io/apiman/manager/api/jpa/JpaStorage.java
+++ manager/api/jpa/src/main/java/io/apiman/manager/api/jpa/JpaStorage.java
@@ -1136,15 +1136,18 @@ public class JpaStorage extends AbstractJpaStorage implements IStorage, IStorage
      * {@inheritDoc}
      */
     @Override
-    public InputStream getApiDefinition(String orgId, String apiId, String apiVersion) throws StorageException {
+    public InputStream getApiDefinition(String apiOrgId, String apiId, String apiVersion) throws StorageException {
         try {
             EntityManager entityManager = getActiveEntityManager();
             String jpql = "SELECT v from ApiDefinitionBean v "
                                   + "JOIN v.apiVersion av "
                                   + "JOIN av.api api "
-                                  + "WHERE api.id = :apiId "
-                                  + "AND av.version = :apiVersion";
+                                  + "JOIN api.organization org "
+                                  + "WHERE org.id = :apiOrgId "
+                                  + "AND api.id = :apiId "
+                                  + "AND av.version = :apiVersion ";
             TypedQuery<ApiDefinitionBean> query = entityManager.createQuery(jpql, ApiDefinitionBean.class)
+                    .setParameter("apiOrgId", apiOrgId)
                     .setParameter("apiId", apiId)
                     .setParameter("apiVersion", apiVersion);
             ApiDefinitionBean apiDef = query.getSingleResult();
