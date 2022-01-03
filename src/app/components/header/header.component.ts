03674fa5e04526bc3bcacb2ca29aea2e9fc550ae
Florian Volk <florian.volk@scheer-group.com>
2022-01-03 11:50:15 +0100

    refactor(config): remove styling information from config file
    This moves all styling information from the config file into the custom css style. That makes configuration easy, and we have to maintain less code to apply styling at runtime

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Scheer PAS Schweiz AG
+ * Copyright 2022 Scheer PAS Schweiz AG
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,14 +14,7 @@
  *  imitations under the License.
  */
 
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  Renderer2,
-  ViewChild
-} from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { HeroService } from '../../services/hero/hero.service';
 import { KeycloakService } from 'keycloak-angular';
 import { KeycloakHelperService } from '../../services/keycloak-helper/keycloak-helper.service';
@@ -32,17 +25,9 @@ import { KeycloakHelperService } from '../../services/keycloak-helper/keycloak-h
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
-  @ViewChild('heroImage') heroImageDiv!: ElementRef;
-  @ViewChild('heroTitle') heroTitle!: ElementRef;
-  @ViewChild('heroSubtitle') heroSubtitle!: ElementRef;
-  @ViewChild('heroOverlay') heroOverlay!: ElementRef;
-  @ViewChild('loginBtn') heroLoginBtn!: ElementRef;
-  @ViewChild('logoutBtn') heroLogoutBtn!: ElementRef;
-
   loggedIn = false;
 
   constructor(
-    private renderer: Renderer2,
     public heroService: HeroService,
     private keycloak: KeycloakService,
     private keycloakHelper: KeycloakHelperService
@@ -53,37 +38,7 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   async ngAfterViewInit(): Promise<void> {
-    this.setStyle(
-      this.heroImageDiv.nativeElement,
-      'background-image',
-      'url("' + (this.heroService.hero.heroImgUrl ?? '') + '")'
-    );
-    if (this.heroService.hero.fontColor) {
-      this.setStyle(
-        this.heroTitle.nativeElement,
-        'color',
-        this.heroService.hero.fontColor.title
-      );
-      this.setStyle(
-        this.heroSubtitle.nativeElement,
-        'color',
-        this.heroService.hero.fontColor.subtitle
-      );
-    }
-
-    this.setStyle(
-      this.heroOverlay.nativeElement,
-      'background-color',
-      this.heroService.hero.overlayColor ?? ''
-    );
-
     this.loggedIn = await this.keycloak.isLoggedIn();
-    this.setStyleForLoginBtn();
-  }
-
-  // eslint-disable-next-line
-  setStyle(element: any, style: string, value: string): void {
-    this.renderer.setStyle(element, style, value);
   }
 
   public login(): void {
@@ -92,33 +47,5 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 
   public logout(): void {
     this.keycloakHelper.logout();
-  }
-
-  private setStyleForLoginBtn(): void {
-    if (this.heroService.hero.buttonColor) {
-      if (this.heroService.hero.buttonColor.login && !this.loggedIn) {
-        this.setStyle(
-          this.heroLoginBtn.nativeElement,
-          'color',
-          this.heroService.hero.buttonColor.login
-        );
-        this.setStyle(
-          this.heroLoginBtn.nativeElement,
-          'border-color',
-          this.heroService.hero.buttonColor.login
-        );
-      } else {
-        this.setStyle(
-          this.heroLogoutBtn.nativeElement,
-          'color',
-          this.heroService.hero.buttonColor.logout
-        );
-        this.setStyle(
-          this.heroLogoutBtn.nativeElement,
-          'border-color',
-          this.heroService.hero.buttonColor.logout
-        );
-      }
-    }
   }
 }
