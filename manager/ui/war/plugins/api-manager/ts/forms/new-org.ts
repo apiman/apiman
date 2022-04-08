d4ad050408eb2188bd2e4c41d0fbaa5952942ee0
Marc Savy <marc@rhymewithgravy.com>
2022-04-08 13:24:47 +0100

    fix(ui): org create button always hidden if not admin
    Fixes: #1890
    
    Co-authored-by: Florian Volk <florian.volk@scheer-group.com>

--- manager/ui/war/plugins/api-manager/ts/forms/new-org.ts
+++ manager/ui/war/plugins/api-manager/ts/forms/new-org.ts
@@ -1,8 +1,8 @@
 import {_module} from "../apimanPlugin";
 
 _module.controller("Apiman.NewOrgController",
-        ['$q', '$location', '$scope', 'OrgSvcs', 'PageLifecycle', 'CurrentUser',
-        function ($q, $location, $scope, OrgSvcs, PageLifecycle, CurrentUser) {
+        ['$q', '$location', '$rootScope', '$scope', 'OrgSvcs', 'PageLifecycle', 'CurrentUser', 'Logger',
+        function ($q, $location, $rootScope, $scope, OrgSvcs, PageLifecycle, CurrentUser, Logger) {
             $scope.saveNewOrg = function() {
                 $scope.createButton.state = 'in-progress';
                 OrgSvcs.save($scope.org, function(reply) {
@@ -10,8 +10,15 @@ _module.controller("Apiman.NewOrgController",
                     PageLifecycle.redirectTo('/orgs/{0}/plans', reply.id);
                 }, PageLifecycle.handleError);
             };
-            
+
             PageLifecycle.loadPage('NewOrg', undefined, undefined, $scope, function() {
+
+                // Using this instead the existing permissions' system, as this is a global setting rather than an org based one
+                if ($rootScope.noCreateOrg === true) {
+                    Logger.info('Only administrators are allowed to create new organizations')
+                    PageLifecycle.handleError({status: 404})
+                }
+
                 PageLifecycle.setPageTitle('new-org');
                 $scope.$applyAsync(function() {
                     $('#apiman-entityname').focus();
