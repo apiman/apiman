d81757f0a21b9b5a8cc84d53a23dd9839f683048
Marc Savy <marc@rhymewithgravy.com>
2022-04-19 17:58:56 +0100

    should have committed this files

--- manager/api/rest-impl/src/main/java/io/apiman/manager/api/rest/impl/ApiResourceImpl.java
+++ manager/api/rest-impl/src/main/java/io/apiman/manager/api/rest/impl/ApiResourceImpl.java
@@ -1,8 +1,6 @@
 package io.apiman.manager.api.rest.impl;
 
 import io.apiman.manager.api.beans.idm.PermissionType;
-import io.apiman.manager.api.beans.search.SearchCriteriaBean;
-import io.apiman.manager.api.beans.search.SearchCriteriaFilterOperator;
 import io.apiman.manager.api.beans.search.SearchResultsBean;
 import io.apiman.manager.api.beans.summary.ApiSummaryBean;
 import io.apiman.manager.api.rest.IApiResource;
@@ -10,9 +8,6 @@ import io.apiman.manager.api.rest.impl.util.PermissionsHelper;
 import io.apiman.manager.api.security.ISecurityContext;
 import io.apiman.manager.api.service.SearchService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
--- manager/api/rest-impl/src/main/java/io/apiman/manager/api/service/SearchService.java
+++ manager/api/rest-impl/src/main/java/io/apiman/manager/api/service/SearchService.java
@@ -4,6 +4,7 @@ import io.apiman.manager.api.beans.idm.OrgsPermissionConstraint;
 import io.apiman.manager.api.beans.idm.RoleBean;
 import io.apiman.manager.api.beans.idm.UserBean;
 import io.apiman.manager.api.beans.search.SearchCriteriaBean;
+import io.apiman.manager.api.beans.search.SearchCriteriaFilterOperator;
 import io.apiman.manager.api.beans.search.SearchResultsBean;
 import io.apiman.manager.api.beans.summary.ApiSummaryBean;
 import io.apiman.manager.api.beans.summary.ClientSummaryBean;
@@ -12,11 +13,10 @@ import io.apiman.manager.api.core.IStorageQuery;
 import io.apiman.manager.api.rest.impl.util.DataAccessUtilMixin;
 import io.apiman.manager.api.rest.impl.util.SearchCriteriaUtil;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Search with permissions and visibility-related constraints.
@@ -27,6 +27,7 @@ import org.jetbrains.annotations.NotNull;
  */
 @ApplicationScoped
 @Transactional
+@ParametersAreNonnullByDefault
 public class SearchService implements DataAccessUtilMixin {
 
     private IStorageQuery query;
@@ -38,29 +39,33 @@ public class SearchService implements DataAccessUtilMixin {
 
     public SearchService() {}
 
-    public SearchResultsBean<OrganizationSummaryBean> findOrganizations(@NotNull SearchCriteriaBean criteria, OrgsPermissionConstraint constraints) {
+    public SearchResultsBean<OrganizationSummaryBean> findOrganizations(SearchCriteriaBean criteria, OrgsPermissionConstraint constraints) {
         SearchCriteriaUtil.validateSearchCriteria(criteria);
         return tryAction(() -> query.findOrganizations(criteria, constraints));
     }
 
-    public SearchResultsBean<ClientSummaryBean> findClients(@NotNull SearchCriteriaBean criteria, OrgsPermissionConstraint constraints) {
+    public SearchResultsBean<ClientSummaryBean> findClients(SearchCriteriaBean criteria, OrgsPermissionConstraint constraints) {
         SearchCriteriaUtil.validateSearchCriteria(criteria);
         return tryAction(() -> query.findClients(criteria, constraints));
     }
 
-    public SearchResultsBean<ApiSummaryBean> findApis(@NotNull SearchCriteriaBean criteria, OrgsPermissionConstraint constraints) {
+    public SearchResultsBean<ApiSummaryBean> findApis(SearchCriteriaBean criteria, OrgsPermissionConstraint constraints) {
         SearchCriteriaUtil.validateSearchCriteria(criteria);
         return tryAction(() -> query.findApis(criteria, constraints, true));
     }
 
-    public SearchResultsBean<UserBean> findUsers(@NotNull SearchCriteriaBean criteria) {
+    public SearchResultsBean<ApiSummaryBean> findAllFeaturedApis(OrgsPermissionConstraint constraints) {
+        SearchCriteriaBean criteria = new SearchCriteriaBean().addFilter("tags.key", "featured", SearchCriteriaFilterOperator.eq);
+        return tryAction(() -> query.findApis(criteria, constraints, false)); // TODO(msavy): when we update/drop H2 change paginate to true -- was generating invalid SQL.
+    }
+
+    public SearchResultsBean<UserBean> findUsers(SearchCriteriaBean criteria) {
         SearchCriteriaUtil.validateSearchCriteria(criteria);
         return tryAction(() -> query.findUsers(criteria));
     }
 
-    public SearchResultsBean<RoleBean> findRoles(@NotNull SearchCriteriaBean criteria) {
+    public SearchResultsBean<RoleBean> findRoles(SearchCriteriaBean criteria) {
         SearchCriteriaUtil.validateSearchCriteria(criteria);
         return tryAction(() -> query.findRoles(criteria));
     }
-
 }
